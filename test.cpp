078515336b00074638664b9b044b7c6e382372b3
Shunichi Miyashita <miyash.shuni.323sat@gmail.com>
2017-08-10 10:51:47 +0900

    C++ test

---
+++ test.cpp
@@ -0,0 +1,15 @@
+// Func1.cpp
+#include <iostream.h>
+
+int f(int x, int y)
+{
+    return 2 * x + y;
+}
+
+int main()
+{
+    cout << "f(1, 2) = " << f(1, 2) << endl;
+    cout << "f(182, 144) = " << f(182, 144) << endl;
+
+    return 0;
+}
